317122b64d33c4b7894077bae26c26f919aa2949
seohq <carolinethebest771@gmail.com>
2026-03-04 15:03:09 +0500

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -27,7 +27,7 @@ function loadFooter() {
                         <li><a href="semantic-seo">Semantic SEO</a></li>
                         <li><a href="content-optimization">Content Optimization</a></li>
                         <li><a href="case-studies">Case Studies</a></li>
-                        <li><a href="serp-simulators">SERP Simulators</a></li>
+                        <li><a href="serp-simulator">SERP Simulator</a></li>
                     </ul>
                 </div>
                 <div class="footer-section">
