eae2ec124f78e26d32a55811468a01bfd470f428
seohq <carolinethebest771@gmail.com>
2026-03-31 14:29:27 +0500

    Update header.js

--- components/header.js
+++ components/header.js
@@ -33,8 +33,7 @@ function loadHeader() {
                             <li><a href="/xml-sitemap-generator" class="dropdown-item">XML Sitemap Generator</a></li>
                             <li><a href="/robots-txt-generator" class="dropdown-item">Robots.txt Generator</a></li>
                             <li><a href="/hreflang-tag-generator" class="dropdown-item">Hreflang Tag Generator</a></li>
-                            <li><a href="/structured-data-validator" class="dropdown-item">Structured Data Validator</a></li>
-                            <li><a href="/mobile-website-viewer" class="dropdown-item">Mobile Website Viewer</a></li>
+                            <li><a href="/structured-data-validator" class="dropdown-item">Structured Data Validator</a></li>                            
                         </ul>
                     </li> 
                     <li><a href="/tech-news" class="nav-link">Tech News</a></li>
